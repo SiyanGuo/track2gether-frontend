6489d8ee9de67a0ac2e49ffc18e1a39740432045
Buhari Nasir Maiwada <maewada@live.com>
2022-04-08 18:05:11 -0400

    created components folder

--- tracker2together/src/app/app.module.ts
+++ tracker2together/src/app/app.module.ts
@@ -7,12 +7,12 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { UserComponent } from './user/user.component';
-import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { UserComponent } from './components/user/user.component';
+import { authInterceptorProviders } from './components/_helpers/auth.interceptor';
 
 
 @NgModule({
