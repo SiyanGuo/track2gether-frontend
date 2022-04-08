0f80d9ce9e72aed30d2803f120e3975549a60733
Serena Guo <syg9345@gmail.com>
2022-04-08 09:39:57 -0400

    add nav-bar and set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { IncomePageComponent } from './components/income-page/income-page.component';
+import { ExpensePageComponent } from './components/expense-page/expense-page.component';
+import { FormComponent } from './components/form/form.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DashboardComponent,
+    IncomePageComponent,
+    ExpensePageComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/expense-page/expense-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-page',
+  templateUrl: './expense-page.component.html',
+  styleUrls: ['./expense-page.component.css']
+})
+export class ExpensePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
