6c9b94fff01e3986efa4ecf1d1f687f1fd83603f
Buhari Nasir Maiwada <maewada@live.com>
2022-04-09 19:21:23 -0400

    completed profile

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,48 +3,52 @@ import { AuthService } from '../_services/auth.service';
 import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: '../login/login.component.html',
-  styleUrls: ['../login/login.component.css']
+  selector: "app-login",
+  templateUrl: "../login/login.component.html",
+  styleUrls: ["../login/login.component.css"],
 })
 export class LoginComponent implements OnInit {
-
   form: any = {
     username: null,
-    password: null
+    password: null,
   };
 
-  isLogginIn = false;
-  isLogginFailed = false;
-  errorMessage = '';
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = "";
   roles: string[] = [];
   // tokenStorage: any;
   // isLogginFailed: boolean;
   // isLogginFailed: boolean | undefined;
   // tokenStorage: any;
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  constructor(
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService
+  ) {}
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
-      this.isLogginIn = true;
+      this.isLoggedIn = true;
       this.roles = this.tokenStorage.getUser().roles;
     }
   }
 
   onSubmit(): void {
     const { username, password } = this.form;
-    this.authService.login(username, password).subscribe(data => {
-      this.tokenStorage.saveToken(data.accessToken);
-      this.tokenStorage.saveUser(data);
-      this.isLogginFailed = false;
-      this.isLogginIn = true;
-      this.roles = this.tokenStorage.getUser().roles;
-      this.reloadPage();
-    }, err => {
-      this.errorMessage = err.error.message;
-      this.isLogginFailed = true;
-    }
+    this.authService.login(username, password).subscribe(
+      (data) => {
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveUser(data);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenStorage.getUser().roles;
+        this.reloadPage();
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
     );
   }
   reloadPage(): void {
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  templateUrl: '../profile/profile.component.html',
+  styleUrls: ['../profile/profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  currentUser: any;
+  constructor(private token: TokenStorageService) { }
 
   ngOnInit(): void {
+     this.currentUser = this.token.getUser();
   }
-
 }
