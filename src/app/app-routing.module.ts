2ed02728c322354e38d1cfbd8584c32d42197da4
Buhari Nasir Maiwada <maewada@live.com>
2022-04-09 20:01:11 -0400

    created app-route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from '../app/components/register/register.component';
+import { LoginComponent } from '../app/components/login/login.component';
+import { HomeComponent } from '../app/components/home/home.component';
+import { ProfileComponent } from '../app/components/profile/profile.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "profile", component: ProfileComponent },
+  // { path: "home", component: HomeComponent },
+  { path: "", redirectTo: "home", pathMatch: "full" }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
