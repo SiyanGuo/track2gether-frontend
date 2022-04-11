8709f7c81412d1dfaac2628a5d0e4ad619363b4c
Buhari Nasir Maiwada <maewada@live.com>
2022-04-10 20:42:58 -0400

    modified app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { TokenStorageService } from './components/_services/token-storage.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'tracker2together';
+export class AppComponent implements OnInit {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  username?: string;
+  title = "tracker2together";
+
+  constructor(private tokenStorageService: TokenStorageService) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.username = user.username;
+    }
+  }
+  logout(): void {
+    this.tokenStorageService.singOut();
+    window.location.reload();
+  }
 }
