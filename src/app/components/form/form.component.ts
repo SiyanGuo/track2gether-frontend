db4581c3f79cb9f32175c35ab18b774bdb1f7722
Serena Guo <syg9345@gmail.com>
2022-04-11 00:38:30 -0400

    populate record in the form for updating

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Record } from 'src/app/models/record';
+import { RecordService } from 'src/app/services/record.service';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -9,28 +10,42 @@ import { Record } from 'src/app/models/record';
 })
 export class FormComponent implements OnInit {
   @Input()
-  type!:string; 
+  type!: string;
 
   @Input()
-  categories!:string[];
+  categories!: string[];
 
   form!: FormGroup;
-  amount!:number;
-  category!:string;
-  date!:string;
-  description!:string;
-  shared!:boolean;
+  amount!: number;
+  category!: string;
+  date!: string;
+  description!: string;
+  shared!: boolean;
 
-  constructor(private formBuilder: FormBuilder) { }
+  currentRecord!: Record;
 
-  addTransaction(formValue:Record){
+  updateRecord() {
+    this.form = this.formBuilder.group({
+      amount: new FormControl(this.currentRecord.amount, [Validators.required, Validators.pattern('^[0-9]+$')]),
+      category: new FormControl(this.currentRecord.category, Validators.required),
+      date: new FormControl(this.currentRecord.date, [Validators.required, Validators.pattern('^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$')]),
+      description: new FormControl(this.currentRecord.description),
+      shared: new FormControl(this.currentRecord.shared)
+    });
+  }
 
-    if(this.form.valid){
-   
+
+  constructor(private formBuilder: FormBuilder, private recordService: RecordService) { }
+
+
+  addTransaction(formValue: Record) {
+
+    if (this.form.valid) {
+
       console.log(formValue);
 
-      if(formValue.shared){
-        formValue.amount /=2;
+      if (formValue.shared) {
+        formValue.amount /= 2;
         // get userId and spouseId, categoryId send two post requests
       } else {
         // get userId, categoryId and send this request
@@ -40,14 +55,20 @@ export class FormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.recordService.currentRecord.subscribe(data => {
+      this.currentRecord = data;
+      this.updateRecord();
+    })
 
     this.form = this.formBuilder.group({
       amount: new FormControl(this.amount, [Validators.required, Validators.pattern('^[0-9]+$')]),
       category: new FormControl(this.category, Validators.required),
       date: new FormControl(this.date, [Validators.required, Validators.pattern('^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$')]),
       description: new FormControl(this.description),
-      shared:new FormControl(this.description)
+      shared: new FormControl(this.shared)
     });
+
+
   }
 
 }
--- src/app/components/record-list/record-list.component.ts
+++ src/app/components/record-list/record-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit, PipeTransform } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { faPencil, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 import { Record } from 'src/app/models/record';
+import { RecordService } from 'src/app/services/record.service';
 
 const RECORDS: Record[] = [
   {
@@ -65,11 +66,16 @@ export class RecordListComponent implements OnInit {
   records$: Observable<Record[]>;
   filter = new FormControl('');
 
-  constructor(pipe: DecimalPipe) {
+  constructor(pipe: DecimalPipe, private recordService: RecordService) {
     this.records$ = this.filter.valueChanges.pipe(
       startWith(''),
       map(text => search(text, pipe))
     );
+  }
+
+  passRecord(record:Record) {
+    this.recordService.currentRecord.next(record);
+    console.log("record",record)
   }
 
   ngOnInit(): void {
