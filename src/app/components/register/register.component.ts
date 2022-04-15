2b3fb2508d7ae6ffa33b54773488046ac218ad6a
Buhari Nasir Maiwada <maewada@live.com>
2022-04-15 10:06:11 -0400

    modified login and register page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,43 +1,34 @@
-// import { Component, OnInit } from '@angular/core';
-// import { AuthService } from '../../services/auth.service';
-
-
-// @Component({
-//   selector: 'app-register',
-//   templateUrl: './register.component.html',
-//   styleUrls: ['./register.component.css']
-// })
-// export class RegisterComponent implements OnInit {
-//   form: any = {
-//     email: null,
-//     firstname: null,
-//     lastname: null,
-//     password: null
-//   };
-
-//   isSuccessful = false;
-//   isSignUpFailed = false;
-//   errorMessage = '';
-
-
-//   constructor(private authService: AuthService) { }
-
-//   ngOnInit(): void {
-//   }
-
-//   onSubmit(): void {
-//     const { firstname, lastname, email, password } = this.form;
-//     this.authService.register(firstname,lastname, email, password).subscribe(
-//       data => {
-//         console.log(data);
-//         this.isSuccessful = true; // disable patch
-//         this.isSignUpFailed = false;
-//       },
-//       err => {
-//         this.errorMessage = err.error.message;
-//         this.isSignUpFailed = true;
-//       }
-//     )
-//   }
-
-// }
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+@Component({
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"],
+})
+export class RegisterComponent implements OnInit {
+  form: any = {
+    firstname: null,
+    lastname: null,
+    email: null,
+    password: null,
+  };
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = "";
+  constructor(private authService: AuthService) {}
+  ngOnInit(): void {}
+  onSubmit(): void {
+    const { firstname, lastname, email, password } = this.form;
+    this.authService.register(firstname, lastname, email, password).subscribe({
+      next: (data) => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      },
+    });
+  }
+}
